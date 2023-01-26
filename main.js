b3341ed61efe9ca7588df0c05625e7d74be22d4a
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2023-01-25 19:52:28 -0800

    added skeleton html, css, and js files

---
+++ main.js
@@ -0,0 +1,12 @@
+// code that should run immediately
+
+
+
+
+// code that needs to wait until DOM is loaded
+document.addEventListener("DOMContentLoaded", function (event) {
+
+});
+
+
+// all functions defined here
