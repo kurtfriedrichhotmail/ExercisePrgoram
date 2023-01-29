95dea27ea98bb98652e126de1cf25c66c36f2157
Afsaneh234 <nina.alaghemand@outlook.com>
2023-01-29 08:07:07 -0800

    changes to the files

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let output = document.getElementById("output");
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("showAllButton").addEventListener("click", function () {
         console.log(exerciseArray);
-        let message = "Exercise  Calorie\n";
+        let message ;
         for (i = 0; i < exerciseArray.length; i++) {
             message = message + exerciseArray[i].situps + "  " + exerciseArray[i].pushups + "  "+ exerciseArray[i].jumprope + "\n";
             console.log(message);
@@ -24,18 +24,33 @@ exerciseArray = []; // define an array of type of exercise
         this.exerciseLenght = length;
         this.exerciseCalories = calories;
         
-    }
+    
+    
+      };
     
     // Create instances of Exercise objects
     // All instances of Exercise will have these common properties and methods
     let situps = new Exercise("Situps", 1 , 10 );
     let pushups = new Exercise("Pushups", 1 , 15);
     let jumprope = new Exercise("Jumprope", 1 , 18);
+
+    
+    document.getElementById("add").onclick = function () {
+        let radioButtonGroup = document.getElementsByName("exercise");
+        let checkedRadio = Array.from(radioButtonGroup).find(
+          (radio) => radio.checked
+        );
+        alert("You have selected : " + checkedRadio.value);
+      };
+
     
     exerciseArray.push(Exercise);
-    console.log(exerciseArray);
+
+        
+
+
     
-    output.innerHTML += situps.exerciseName + " " + situps.exerciseLenght + " " + situps.calories+ "<br><br>";
+    output.innerHTML += situps.exerciseName + " " + situps.exerciseLenght + " " + situps.exerciseCalories+ "<br><br>";
     output.innerHTML += pushups.exerciseName + " " + pushups.exerciseLenght + " " + pushups.exerciseCalories + "<br><br>";
     output.innerHTML += jumprope.exerciseName + "  " + jumprope.exerciseLenght + "  " + jumprope.exerciseCalories
 
