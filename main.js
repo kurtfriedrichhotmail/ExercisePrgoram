487d143bdebc3262f3945b8124a3e38d0991c959
Afsaneh234 <nina.alaghemand@outlook.com>
2023-01-29 21:36:05 -0800

    changes made by Kurt

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ let Exercise = function  (which, length, calories) {
 
 // code that needs to wait until DOM is loaded
 document.addEventListener("DOMContentLoaded", function (event) {
-<<<<<<< HEAD
   document.getElementById("showAllButton").addEventListener("click", function () {
     console.log(exerciseArray);
     let message ;
@@ -71,57 +70,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
   
-=======
-  let output = document.getElementById("output"); 
-
-
-    // Create instances of Exercise objects
-    // All instances of Exercise will have these common properties and methods
-    let situps = new Exercise("Situps", 1 , 10 );
-    exerciseArray.push(situps);
-
-    let pushups = new Exercise("Pushups", 1 , 15);
-    exerciseArray.push(pushups);
-
-    let jumprope = new Exercise("Jumprope", 1 , 18);
-    exerciseArray.push(jumprope);
-
-  
-
-    document.getElementById("add").onclick = function () {
-
-      let time = document.getElementById("time").value;
-      let calories = 0;
-      let radioButtonGroup = document.getElementsByName("exercise");
-      let checkedRadio = Array.from(radioButtonGroup).find(
-        (radio) => radio.checked
-      );
-      console.log("here");
-        console.log(checkedRadio.value);
-      if(checkedRadio.value === "Sit Ups"){
-        calories = time * 10;
-      }
-      if(checkedRadio.value === "Push Ups"){
-         calories = time * 15;
-      }
-      if(checkedRadio.value === "Jump Rope"){
-          calories = time * 18;
-      }
-      exerciseArray.push(new Exercise(checkedRadio.value, time, calories))
-      };
-
- 
-
-    document.getElementById("showAllButton").addEventListener("click", function () {
-        let ul = document.getElementById('output');
-        ul.innerHTML = "";
-        for (let i = 0; i < exerciseArray.length; i++) {
-            let li = document.createElement('li');
-            ul.appendChild(li);
-            li.innerHTML =  exerciseArray[i].exerciseName + "   " +  exerciseArray[i].exerciseLenght + " minutes " +   exerciseArray[i].exerciseCalories + " calories";
-        };
-    });
->>>>>>> 8ea6bb7b8a1db09cfba6f2166012c8231714eb05
       
        
     
