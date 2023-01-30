0007c83907d7c79f6ef7ec2ddb28e22192a32b3f
Afsaneh234 <nina.alaghemand@outlook.com>
2023-01-30 14:37:21 -0800

    Final submit

--- main.js
+++ main.js
@@ -34,39 +34,58 @@ document.addEventListener("DOMContentLoaded", function (event) {
       // end object constructor
       };
     
+
+
+    let output = document.getElementById("output"); 
+
+
     // Create instances of Exercise objects
     // All instances of Exercise will have these common properties and methods
-    let situps = new WorkOut("situps",  1 , 10 );
-    let pushups = new WorkOut("pushups", 1 , 15);
-    let jumprope = new WorkOut("jumprope", 1 , 18);
+    let situps = new Exercise("Situps", 1 , 10 );
+    exerciseArray.push(situps);
+
+    let pushups = new Exercise("Pushups", 1 , 15);
+    exerciseArray.push(pushups);
+
+    let jumprope = new Exercise("Jumprope", 1 , 18);
+    exerciseArray.push(jumprope);
 
 
-
-
-
-
-
-
-   
 
     document.getElementById("add").onclick = function () {
+
+      let time = document.getElementById("time").value;
+      let calories = 0;
       let radioButtonGroup = document.getElementsByName("exercise");
       let checkedRadio = Array.from(radioButtonGroup).find(
         (radio) => radio.checked
       );
-      alert("You have selected : " + checkedRadio.value);
-    };
-  
+      console.log("here");
+        console.log(checkedRadio.value);
+      if(checkedRadio.value === "Sit Ups"){
+        calories = time * 10;
+      }
+      if(checkedRadio.value === "Push Ups"){
+         calories = time * 15;
+      }
+      if(checkedRadio.value === "Jump Rope"){
+          calories = time * 18;
+      }
+      exerciseArray.push(new Exercise(checkedRadio.value, time, calories))
+      };
 
-     
-    exerciseArray.push(WorkOut);
-   
-        
 
 
-    output.innerHTML += situps+ " " + length + " " + calories + "<br><br>";
-    output.innerHTML += pushups + " " + length  + " " + calories + "<br><br>";
-    output.innerHTML += jumprope+ "  " + length + "  " + calories
+    document.getElementById("showAllButton").addEventListener("click", function () {
+        let ul = document.getElementById('output');
+        ul.innerHTML = "";
+        for (let i = 0; i < exerciseArray.length; i++) {
+            let li = document.createElement('li');
+            ul.appendChild(li);
+            li.innerHTML =  exerciseArray[i].exerciseName + "   " +  exerciseArray[i].exerciseLenght + " minutes " +   exerciseArray[i].exerciseCalories + " calories";
+        };
+    });
+
 
 
   
