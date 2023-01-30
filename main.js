9c1950b5dc2b3ebf303f5017594bbab771f8f826
Afsaneh234 <nina.alaghemand@outlook.com>
2023-01-29 21:18:34 -0800

    changes made to index.html and main.js

--- main.js
+++ main.js
@@ -5,56 +5,65 @@ let output = document.getElementById("output");
 
 // code that needs to wait until DOM is loaded
 document.addEventListener("DOMContentLoaded", function (event) {
-  
+  document.getElementById("showAllButton").addEventListener("click", function () {
+    console.log(exerciseArray);
+    let message ;
+    for (i = 0; i < exerciseArray.length; i++) {
+        message = message + exerciseArray[i].situps + "  " + exerciseArray[i].pushups + "  "+ exerciseArray[i].jumprope + "\n";
+        console.log(message);
+    }
+    document.getElementById("output").value = message;
+});
 
+});
     exerciseArray = []; // define an array of type of exercise
 
-    let Exercise = function  (which, length, calories) {
-        this.exerciseName = which;
-        this.exerciseLenght = length;
-        this.exerciseCalories = calories;
-        
-
+    let WorkOut = function  (pWhich, pLength, pCalories) {
+        this.which = pWhich;
+        this.length = parseFloat (pLength);
+        this.calories =parseFloat (pCalories);
+        this.calculateAdd = function () {
+          return this.length * this.calories;
+        };
       // end object constructor
       };
     
     // Create instances of Exercise objects
     // All instances of Exercise will have these common properties and methods
-    let situps = new Exercise("Situps", 1 , 10 );
-    let pushups = new Exercise("Pushups", 1 , 15);
-    let jumprope = new Exercise("Jumprope", 1 , 18);
+    let situps = new WorkOut("situps",  1 , 10 );
+    let pushups = new WorkOut("pushups", 1 , 15);
+    let jumprope = new WorkOut("jumprope", 1 , 18);
 
-    exerciseArray.push(Exercise);
 
-    document.getElementById("add").onclick = function () {
-        let exercise1 = document.getElementsByName("exercise");
-        let checkedRadio = Array.from(exercise1).find(
-          (radio) => radio.checked
-        );
-        alert("You have selected : " + checkedRadio.value);
-      };
 
-     
+
+
+
+
+
    
 
+    document.getElementById("add").onclick = function () {
+      let radioButtonGroup = document.getElementsByName("exercise");
+      let checkedRadio = Array.from(radioButtonGroup).find(
+        (radio) => radio.checked
+      );
+      alert("You have selected : " + checkedRadio.value);
+    };
+  
+
+     
+    exerciseArray.push(WorkOut);
+   
         
 
 
-    
-    output.innerHTML += situps.exerciseName + " " + situps.exerciseLenght + " " + situps.exerciseCalories+ "<br><br>";
-    output.innerHTML += pushups.exerciseName + " " + pushups.exerciseLenght + " " + pushups.exerciseCalories + "<br><br>";
-    output.innerHTML += jumprope.exerciseName + "  " + jumprope.exerciseLenght + "  " + jumprope.exerciseCalories
+    output.innerHTML += situps+ " " + length + " " + calories + "<br><br>";
+    output.innerHTML += pushups + " " + length  + " " + calories + "<br><br>";
+    output.innerHTML += jumprope+ "  " + length + "  " + calories
 
 
-    document.getElementById("showAllButton").addEventListener("click", function () {
-        console.log(exerciseArray);
-        let message ;
-        for (i = 0; i < exerciseArray.length; i++) {
-            message = message + exerciseArray[i].situps + "  " + exerciseArray[i].pushups + "  "+ exerciseArray[i].jumprope + "\n";
-            console.log(message);
-        }
-        document.getElementById("output").value = message;
-    });
+  
       
        
     document.getElementById("situps").value = "";
@@ -62,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("jumprope").value = "";
 
     
-});
+
 
 
 
